5b695813435c8376cdbacdf00dbaa0aa2a5c6b27
Bruno de Biazio <biazzioxbox@gmail.com>
2026-03-04 12:29:04 -0300

    corrigido whatsapp

--- react/botaowhatsapp.tsx
+++ react/botaowhatsapp.tsx
@@ -3,7 +3,10 @@ import React, { useState } from "react"
 function BotaoWhatsapp() {
   const [isHover, setIsHover] = useState(false)
 
-  const href = `https://w.app/btjsqb`
+  // const telefone = "554198516332"
+  const telefone = "5541985360075"
+  const mensagem = "Olá Idealine \n Gostaria de ser atendida por uma consultora sobre os produtos!"
+  const href = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`
 
   const iconUrl =
     "https://mfmgroup.vtexassets.com/assets/vtex.file-manager-graphql/images/b01af4d9-4dc4-49a4-b902-217cf2fbcc20___09fd6f73b641157626626f4bdde27deb.svg"
@@ -32,12 +35,8 @@ function BotaoWhatsapp() {
         zIndex: 9999,
         textDecoration: "none",
         cursor: "pointer",
-
-        // Transparência
         opacity: isHover ? 1 : 0.5,
         transition: "opacity 200ms ease, box-shadow 200ms ease",
-
-        // Sombra estilo da imagem (suave, pouco offset)
         boxShadow: "0 6px 14px rgba(0,0,0,0.18)",
       }}
     >
